1f34e625b04d429015a2aef875752182e9d6f0bb
zealotry1developer <mmyroncaldwell@gmail.com>
2022-07-01 22:44:08 -0500

    Update OnBoardingPage.swift

--- omega-eCommerce/View/OnBoardingPage.swift
+++ omega-eCommerce/View/OnBoardingPage.swift
@@ -7,11 +7,47 @@
 
 import SwiftUI
 
+// Custom fonts on all pages
+let customFont = "Raleway-Regular"
+
 struct OnBoardingPage: View {
     var body: some View {
-        VStack{
+        // Hero section
+        VStack(alignment: .leading){
+            // Hero text
+            Text("Find your \nGadget")
+                .font(.custom(customFont, size: 55))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
             
+            // Hero image
+            Image("OnBoard")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            // Hero button
+            Button  {
+                
+            } label: {
+                Text("Get Started")
+                    .font(.custom(customFont, size: 18))
+                    .fontWeight(.bold)
+                    .padding(.vertical, 18)
+                    .frame(maxWidth: .infinity)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.1), radius: 5, x: 5, y: 5)
+                    .foregroundColor(Color("Purple"))
+            }
+            .padding(.horizontal, 30)
+            // adding some adjusments only for larger displays
+            .offset(y: getRect().height < 750 ? 20: 40)
+            
+            Spacer()
+
         }
+        .padding()
+        .padding(.top, getRect().height < 750 ? 0 : 20)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(
             Color("Purple")
@@ -22,5 +58,14 @@ struct OnBoardingPage: View {
 struct OnBoardingPage_Previews: PreviewProvider {
     static var previews: some View {
         OnBoardingPage()
+    }
+}
+
+// Extend View to get screen bounds
+extension View {
+    // get rectangle
+    func getRect() -> CGRect{
+        // CGRect contains locations and dimensions of a rectangle
+        return UIScreen.main.bounds
     }
 }
