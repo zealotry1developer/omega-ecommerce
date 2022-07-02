bf0a20e14a34a46669d9ed3dbca13fd7a3e589d2
zealotry1developer <mmyroncaldwell@gmail.com>
2022-07-01 22:18:31 -0500

    Init OnBoardingPage

---
+++ omega-eCommerce/View/OnBoardingPage.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoardingPage.swift
+//  omega-eCommerce
+//
+//  Created by Logan Nguyễn on 7/1/22.
+//
+
+import SwiftUI
+
+struct OnBoardingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoardingPage_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingPage()
+    }
+}
