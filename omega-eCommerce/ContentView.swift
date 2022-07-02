18718bac86e27110617cf21e5a539df230bed0c6
zealotry1developer <mmyroncaldwell@gmail.com>
2022-07-01 21:56:14 -0500

    Initial Commit

---
+++ omega-eCommerce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  omega-eCommerce
+//
+//  Created by Logan Nguyễn on 7/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ omega-eCommerce/omega_eCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  omega_eCommerceApp.swift
+//  omega-eCommerce
+//
+//  Created by Logan Nguyễn on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct omega_eCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
