f6757ef09c2e769bae51e6b4a65b089ee02182f6
zealotry1developer <mmyroncaldwell@gmail.com>
2022-07-03 01:43:44 -0500

    Setup LoginPage and LoginPageModel

---
+++ omega-eCommerce/View/LoginPage.swift
@@ -0,0 +1,20 @@
+//
+//  LoginPage.swift
+//  omega-eCommerce
+//
+//  Created by Logan Nguyễn on 7/3/22.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginPage_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginPage()
+    }
+}
---
+++ omega-eCommerce/ViewModel/LoginPageModel.swift
@@ -0,0 +1,20 @@
+//
+//  LoginPageModel.swift
+//  omega-eCommerce
+//
+//  Created by Logan Nguyễn on 7/3/22.
+//
+
+import SwiftUI
+
+struct LoginPageModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginPageModel_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginPageModel()
+    }
+}
