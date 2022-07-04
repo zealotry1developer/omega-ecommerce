fcd7f30cfd2562c70da2308a65f1a65793e8de5a
zealotry1developer <mmyroncaldwell@gmail.com>
2022-07-04 12:33:49 -0500

    Added ProductCardView child view to HomeView

--- omega-eCommerce/View/TabViews/Home.swift
+++ omega-eCommerce/View/TabViews/Home.swift
@@ -96,8 +96,36 @@ struct Home: View {
         }
     }
     
+    @ViewBuilder
+    func ProductCardView(product: Product) -> some View {
+        VStack(spacing: 10) {
+            Image(product.productImage)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: getRect().width / 2.5, height: getRect().height / 4.5)
+                .offset(y: -80)
+                .padding(.bottom, -80)
+            
+            Text(product.title)
+                .font(.custom(customFont, size: 18))
+                .fontWeight(.semibold)
+                .padding(.top)
+
+            Text(product.subtitle)
+                .font(.custom(customFont, size: 14))
+                .foregroundColor(Color.gray)
+
+            Text(product.price)
+                .font(.custom(customFont, size: 16))
+                .fontWeight(.semibold)
+                .foregroundColor(Color("Purple"))
+                .padding(.top)
+        }
+        .padding(.horizontal, 20)
+        .padding(.bottom, 22)
         .background(
-            Color("HomeBG")
+            Color.white
+                .cornerRadius(25)
         )
     }
 }
