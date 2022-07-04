3cec36c08373f701abef0f28348c552bd772dfa5
zealotry1developer <mmyroncaldwell@gmail.com>
2022-07-03 19:31:33 -0500

    Created mainpage view

---
+++ omega-eCommerce/View/MainPage.swift
@@ -0,0 +1,21 @@
+//
+//  MainPage.swift
+//  omega-eCommerce
+//
+//  Created by Logan Nguyễn on 7/3/22.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainPage_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPage()
+    }
+}
+
