76bb42549918536baa4d6cc407499af74a2dcd6c
zealotry1developer <mmyroncaldwell@gmail.com>
2022-07-04 01:28:00 -0500

    Added Product struct to ProductModel

--- omega-eCommerce/Model/ProductModel.swift
+++ omega-eCommerce/Model/ProductModel.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 
+// Product Model
+struct Product: Identifiable,Hashable {
+    var id = UUID().uuidString
+    var type: ProductType
+    var title: String
+    var subtitle: String
+    var description: String = ""
+    var price: String
+    var productImage: String = ""
+    var quantity: Int = 1
+}
+
 // Product Types
 enum ProductType: String, CaseIterable {
-    case Weareable = "Weareable"
+    case Wearable = "Wearable"
     case Laptops = "Laptops"
     case Phones = "Phones"
     case Tablets = "Tablets"
