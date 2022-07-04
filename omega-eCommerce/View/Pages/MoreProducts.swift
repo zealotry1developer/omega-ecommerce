fe1d6ffb2ec1714405027155384eed989468aab8
zealotry1developer <mmyroncaldwell@gmail.com>
2022-07-04 12:39:24 -0500

    Files refactored

---
+++ omega-eCommerce/View/Pages/MoreProducts.swift
@@ -0,0 +1,20 @@
+//
+//  MoreProducts.swift
+//  omega-eCommerce
+//
+//  Created by Logan Nguyễn on 7/4/22.
+//
+
+import SwiftUI
+
+struct MoreProducts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoreProducts_Previews: PreviewProvider {
+    static var previews: some View {
+        MoreProducts()
+    }
+}
