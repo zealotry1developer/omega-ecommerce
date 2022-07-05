fd84ca5fc4c174ce81ff4d87c97dc6aab47a2969
zealotry1developer <mmyroncaldwell@gmail.com>
2022-07-04 20:46:30 -0500

    Added TitleBar, Product Image, Product details info to ProductDetailPage

--- omega-eCommerce/View/Pages/ProductDetailPage.swift
+++ omega-eCommerce/View/Pages/ProductDetailPage.swift
@@ -8,13 +8,129 @@
 import SwiftUI
 
 struct ProductDetailView: View {
+    var product: Product
+    
+    // Shared Data Model
+    @EnvironmentObject var sharedData: SharedDataViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+            // Title Bar and Product Image
+            VStack {
+                
+                // Title Bar
+                HStack {
+                    
+                    // Back button
+                    Button {
+                        // Closing View
+                        withAnimation(.easeInOut) {
+//                            sharedData.showDetailProduct == false
+                        }
+                        
+                    } label: {
+                        Image(systemName: "arrow.left")
+                            .font(.title2)
+                            .foregroundColor(Color.black.opacity(0.7))
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image("Liked")
+                            .renderingMode(.template)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 22, height: 22)
+                            .foregroundColor(Color.black.opacity(0.7))
+                    }
+                    
+                }
+                .padding()
+                
+                // Product Image
+                Image(product.productImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.horizontal)
+                    .offset(y: -12)
+                    .frame(maxHeight: .infinity)
+                
+                
+            }
+            .frame(height: getRect().height / 2.7)
+            
+            // Product Details
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                // Product Data
+                VStack(alignment: .leading, spacing: 15) {
+                    Text(product.title)
+                        .font(.custom(customFont, size: 20).bold())
+                    
+                    Text(product.subtitle)
+                        .font(.custom(customFont, size: 18).bold())
+                        .foregroundColor(.gray)
+                    
+                    Text("Get Apple TV+ free for a year")
+                        .font(.custom(customFont, size: 16).bold())
+                        .padding(.top)
+                    
+                    Text("Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, $4.99/month after free trial.")
+                        .font(.custom(customFont, size: 15))
+                        .foregroundColor(.gray)
+                    
+                    // Full desc. btn
+                    Button {
+                        
+                    } label: {
+
+                        Label {
+                            Image(systemName: "arrow.right")
+                        } icon: {
+                            Text("Full description")
+                        }
+                        .font(.custom(customFont, size: 15).bold())
+                        .foregroundColor(Color("Purple"))
+                    }
+                    
+                    // Total section
+                    HStack {
+                        
+                        Text("Total")
+                            .font(.custom(customFont, size: 17))
+                        
+                        Spacer()
+                        
+                        Text("\(product.price)")
+                            .font(.custom(customFont, size: 20).bold())
+                            .foregroundColor(Color("Purple"))
+                    }
+                    .padding(.vertical, 20)
+                }
+                .padding([.horizontal, .bottom], 20)
+                .padding(.top, 25)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(
+                Color.white
+                    .clipShape(CustomCorners(corners: [.topLeft, .topRight], radius: 25))
+                    .ignoresSafeArea()
+            )
+        }
+        .background(Color("HomeBG").ignoresSafeArea())
     }
 }
 
 struct ProductDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductDetailView()
+        ProductDetailView(product: HomeViewModel().products[0])
+            .environmentObject(SharedDataViewModel())
     }
 }
