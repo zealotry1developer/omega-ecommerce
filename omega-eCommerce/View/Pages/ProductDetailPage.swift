de81e4c0727da98fdc8483b0cdfc15c9cef1d3bf
zealotry1developer <mmyroncaldwell@gmail.com>
2022-07-04 20:11:16 -0500

    File reconstructed

---
+++ omega-eCommerce/View/Pages/ProductDetailPage.swift
@@ -0,0 +1,20 @@
+//
+//  ProductDetailView.swift
+//  omega-eCommerce
+//
+//  Created by Logan Nguyễn on 7/4/22.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView()
+    }
+}
